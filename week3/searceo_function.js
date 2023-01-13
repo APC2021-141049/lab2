ae4bda97a1f2a0f43ee5f8c68fd576d249e31056
Sean Aldrich E. Arceo <searceo@student.apc.edu.ph>
2023-01-13 15:45:55 +0800

    New Files, JavaScript Lesson - 1.2, JavaScript Lesson - 2.1, JavaScript Lesson - 9.4, JavaScript Lesson 5.1

--- week3/searceo_function.js
+++ week3/searceo_function.js
@@ -1,22 +1,56 @@
 //Backend Function//
 var port_works = document.getElementById("porter_audio"); 
+var hiddenbooey = document.getElementById("hidehobby_booey");
+
 port_works.volume = 0.7;
 
+port_song = 0;
+mum_song = 0;
+
 function playAudio1() { 
-	port_works.play(); 
+	port_song = 1;
+	port_works.play();
+	if (mum_song == 1){
+		document.getElementById("profile_img2").src="mumport_picture.png";
+	} else {
+	document.getElementById("profile_img2").src="porter_picture.png";
+	}
 } 
 
-function pauseAudio1() { 
-	port_works.pause(); 
+function pauseAudio1() {
+	port_song = 0;
+	port_works.pause();
+	if (mum_song == 1){
+		document.getElementById("profile_img2").src="mumei_picture.png";
+	} else {
+	document.getElementById("profile_img2").src="profile_picture.png";
+	}
 }
 
 var mum_works = document.getElementById("mumei_audio"); 
 mum_works.volume = 0.7;
 
 function playAudio2() { 
-	mum_works.play(); 
+	mum_song = 1;
+	mum_works.play();
+	if (port_song == 1){
+		document.getElementById("profile_img2").src="mumport_picture.png";
+	} else {
+	document.getElementById("profile_img2").src="mumei_picture.png";
+	}
 } 
 
 function pauseAudio2() { 
-	mum_works.pause(); 
+	mum_song = 0;
+	mum_works.pause();
+	if (port_song == 1){
+		document.getElementById("profile_img2").src="porter_picture.png";
+	} else {
+	document.getElementById("profile_img2").src="profile_picture.png";
+	}
 }
+
+function playAudio3() {
+	hiddenbooey.play();
+}
+	
